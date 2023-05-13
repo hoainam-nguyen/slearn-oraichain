f05e247199d60b9d9faf5cff284031ad7935baf0
lynguyenminh <20521592@gm.uit.edu.vn>
2023-05-14 06:29:38 +0700

    Update: header forum

--- treact/src/components/chatbot/LanguageGPT.js
+++ treact/src/components/chatbot/LanguageGPT.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
-import tw, { styled } from 'twin.macro';
+import React, { useState, useEffect, useRef } from "react";
+import tw, { styled } from "twin.macro";
 import { useParams } from "react-router-dom";
 import Header from "../headers/light.js";
 import Footer from "../footers/FiveColumnWithInputForm.js";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 // import { Spinner } from 'react-bootstrap';
+import "./styles.css";
 
 export const ChatbotContainer = styled.div`
-  ${tw`rounded-t-lg
+    ${tw`rounded-t-lg
   m-4
   border-2
   border-gray-300
@@ -61,99 +62,115 @@ const TextReq = tw.div`
     rounded-lg
     m-2
     p-2
-`
+`;
 const Message = tw.div`p-2 my-2 mx-auto max-w-md rounded-xl shadow-md`;
 const TextRep = tw.div`
     rounded-lg
     m-2
     p-2
-`
-function postData(url = '', data = {}) {
-  return fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => response.json());
+`;
+function postData(url = "", data = {}) {
+    return fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.json());
 }
 
-const Chatbot = ({type="slearn"}) => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const Chatbot = ({ type = "slearn" }) => {
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const containerRef = useRef(null);
+    const containerRef = useRef(null);
 
-  const handleChange = (e) => {
-    setMessage(e.target.value);
-  };
+    const handleChange = (e) => {
+        setMessage(e.target.value);
+    };
 
-  useEffect(() => {
-    const container = containerRef.current;
-    container.scrollTo(0, container.scrollHeight);
-  }, [messages]);
+    useEffect(() => {
+        const container = containerRef.current;
+        container.scrollTo(0, container.scrollHeight);
+    }, [messages]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    if (message !== '') {
-      setMessages([...messages, { sender: 'user', text: message }, { sender: 'bot', text: "Loading ..." }])
-      setMessage('');
-      setIsLoading(true);
-      
+        if (message !== "") {
+            setMessages([
+                ...messages,
+                { sender: "user", text: message },
+                { sender: "bot", text: "Loading ..." },
+            ]);
+            setMessage("");
+            setIsLoading(true);
 
-      
+            const data = await postData(
+                "https://aiclub.uit.edu.vn/namnh/multi_bot/chat",
+                { bot_id: type, prompt: message }
+            );
+            // console.log(data)
+            if (data.msg == "Finish!" && data.status_code == 200) {
+                setMessages([
+                    ...messages,
+                    { sender: "user", text: message },
+                    { sender: "bot", text: data.content.answer },
+                ]);
+            } else {
+                setMessages([
+                    ...messages,
+                    { sender: "user", text: message },
+                    { sender: "bot", text: `ERROR: ${data}` },
+                ]);
+            }
+            setIsLoading(false);
+            // console.log(message);
+            // Call API to send message and get response
+            // Add response to messages state
+        }
+    };
 
-      const data = await postData('https://aiclub.uit.edu.vn/namnh/multi_bot/chat', {bot_id:type,  prompt: message })
-      // console.log(data)
-      if (data.msg == 'Finish!' && data.status_code == 200)
-      {
-        setMessages([...messages, { sender: 'user', text: message }, { sender: 'bot', text: data.content.answer }]);
-      }
-      else{
-        setMessages([...messages, { sender: 'user', text: message }, { sender: 'bot', text: `ERROR: ${data}` }]);
-      }
-        setIsLoading(false);
-      // console.log(message);
-      // Call API to send message and get response
-      // Add response to messages state
-    }
-  };
-
-  return (
-    <AnimationRevealPage>
-      <Header />
-      <ChatbotContainer>
-        <ChatHeader>
-          <ChatTitle>EduBot</ChatTitle>
-        </ChatHeader>
-        <ChatBody ref={containerRef}>
-          {messages.map((message, index) => (
-            <div key={index}>
-              {message.sender === 'user' ? (
-                <TextReq>
-                  <span>You:</span> {message.text}
-                </TextReq>
-              ) : (
-                <TextRep>
-                  <span></span> {message.text}
-                </TextRep>
-              )}
+    return (
+        <AnimationRevealPage>
+            <Header />
+            <div className="chatbot">
+                <ChatbotContainer>
+                    <ChatHeader>
+                        <ChatTitle>EduBot</ChatTitle>
+                    </ChatHeader>
+                    <ChatBody ref={containerRef}>
+                        {messages.map((message, index) => (
+                            <div key={index}>
+                                {message.sender === "user" ? (
+                                    <TextReq>
+                                        <span>You:</span> {message.text}
+                                    </TextReq>
+                                ) : (
+                                    <TextRep>
+                                        <span></span> {message.text}
+                                    </TextRep>
+                                )}
+                            </div>
+                        ))}
+                    </ChatBody>
+                    <form onSubmit={handleSubmit}>
+                        <ChatInput
+                            type="text"
+                            value={message}
+                            placeholder="Write question here ..."
+                            onChange={handleChange}
+                        />
+                        <ChatButton type="submit">Send</ChatButton>
+                    </form>
+                </ChatbotContainer>
             </div>
-          ))}
-        </ChatBody>
-        <form onSubmit={handleSubmit}>
-          <ChatInput type="text" value={message} placeholder="Write question here ..." onChange={handleChange} />
-          <ChatButton type="submit">Send</ChatButton>
-        </form>
-      </ChatbotContainer>
-      <Footer />
-    </AnimationRevealPage>
-  )
-}
+            <Footer />
+        </AnimationRevealPage>
+    );
+};
 
 export default Chatbot;
